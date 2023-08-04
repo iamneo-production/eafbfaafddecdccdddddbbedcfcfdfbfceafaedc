66f3accc6145fc4f1701ea3ad753ff2523dbe8a9
Iamneo <techadmin@iamneo.ai>
2023-08-04 12:17:38 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/TaskRepository.java
+++ springapp/src/main/java/com/examly/springapp/TaskRepository.java
@@ -1,13 +1,11 @@
 package com.examly.springapp;
 // Importing required classes
-import com.amiya.springbootdemoproject.entity.Department;
-import com.examl
+import com.examly.springapp.Task;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
  
 // Annotation
 @Repository
- 
 // Class
 public interface TaskRepository
     extends CrudRepository<Department, Long> {
