d9bcc893a65dbd20a12b59560d17940eb03f2472
Iamneo <techadmin@iamneo.ai>
2023-08-04 10:37:37 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/TaskController.java
+++ springapp/src/main/java/com/examly/springapp/TaskController.java
@@ -1,5 +1,16 @@
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class TaskController {
     
+    @PostMapping("/saveTask")
+	public Employee saveTask(@RequestBody Employee emp) {
+		repository.store(emp);
+		return emp;
+	}
 }
